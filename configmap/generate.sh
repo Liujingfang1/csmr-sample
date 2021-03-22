a4b8873feeb4f0aafe309ea21e7992bc2cdd8bc7
Jingfang Liu <jingfangliu@google.com>
2021-03-22 11:35:46 -0700

    add 100 configmap

---
+++ configmap/generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for num in {1..100}
+do
+  cat > configmap_$num.yaml <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: configmap-$num
+  namespace: ns-$num
+data:
+  foo: bar
+  key: value-$num
+EOF
+done
