88ec5fbb0a360d8dfb0c37fef242e0554c9fd95d
Jingfang Liu <jingfangliu@google.com>
2021-03-23 13:48:17 -0700

    udpate resources

--- configmap/generate.sh
+++ configmap/generate.sh
@@ -2,12 +2,13 @@
 
 for num in {1..2000}
 do
+  nm=$(($num/4))
   cat > configmap_$num.yaml <<EOF
 apiVersion: v1
 kind: ConfigMap
 metadata:
   name: configmap-$num
-  namespace: ns-$num
+  namespace: ns-$nm
 data:
   foo: bar
   key: value-$num
