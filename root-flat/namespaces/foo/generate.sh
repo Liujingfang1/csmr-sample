9d003a124487e1633ce86fe092c68ef0f2ced4b8
Jingfang Liu <jingfangliu@google.com>
2021-04-02 10:35:08 -0700

    keep only 4 secret

--- root-flat/namespaces/foo/generate.sh
+++ root-flat/namespaces/foo/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for num in {1..4000}
+for num in {1..4}
 do
   cat > secret_$num.yaml <<EOF
 apiVersion: v1
@@ -8,7 +8,6 @@ kind: Secret
 metadata:
   name: secret-$num
   namespace: foo
-type: kubernetes.io/service-account-token
 data:
   extra: YmFyCg==
 EOF
